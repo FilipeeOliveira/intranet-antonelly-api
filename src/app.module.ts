bf479faa9b7897973cf7751a05c0bd2ccd016850
yluiz <luizvictor1231@gmail.com>
2025-09-28 19:41:28 -0400

    refactor: reorganize module imports in app.module.ts for clarity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
-import { PrismaModule } from './modules/prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { UsersModule } from './modules/users/users.module';
-import { LoggingInterceptor } from './shared/interceptors/logging.interceptor';
+import { CompaniesModule } from './modules/companies/companies.module';
 import { DocumentsModule } from './modules/documents/documents.module';
 import { PermissionsModule } from './modules/permissions/permissions.module';
-import { PermissionsGuard } from './modules/permissions/presentation/guards/permissions.guard';
+import { PrismaModule } from './modules/prisma/prisma.module';
 import { SectorsModule } from './modules/sectors/sectors.module';
+import { UsersModule } from './modules/users/users.module';
+import { VisitHistoryModule } from './modules/visit-history/visit-history.module';
 import { VisitorsModule } from './modules/visitors/visitors.module';
-import { VisitHistoryModule } from './modules/visitor-history/visit-history.module';
-import { CompaniesModule } from './modules/companies/companies.module';
+import { LoggingInterceptor } from './shared/interceptors/logging.interceptor';
 
 @Module({
   imports: [
