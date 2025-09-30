fae81217bf27a308b34d487a0e9da519e2bd8b95
filipe.oliveira <filipe.jx.10@gmail.com>
2025-09-30 12:32:38 -0400

    feat: add sector validation to document creation and update processes

--- src/modules/documents/documents.module.ts
+++ src/modules/documents/documents.module.ts
@@ -3,12 +3,14 @@ import { DocumentsService } from './application/services/documents.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { DocumentRepository } from './infrastructure/repositories/document.repository';
 import { DocumentsController } from './presentation/controllers/documents.controller';
+import { SectorRepository } from '../sectors/infrastructure/repositories/sector.repository';
 
 @Module({
     imports: [PrismaModule],
     controllers: [DocumentsController],
     providers: [
         DocumentRepository,
+        SectorRepository,
         DocumentsService,
     ],
 })
