15a7108bf7f259b0900bb10d9fe47d9163f513be
yluiz <luizvictor1231@gmail.com>
2026-02-16 00:20:58 -0400

    fix: adjust error of timezone in docker

--- dockerfile
+++ dockerfile
@@ -1,6 +1,8 @@
 FROM node:22.11.0-alpine AS development
 
-RUN apk update && apk add --no-cache git bash curl
+RUN apk update && apk add --no-cache git bash curl tzdata
+
+ENV TZ=America/Manaus
 
 WORKDIR /home/node/app
 
--- src/modules/visit-history/application/services/visit-history.service.ts
+++ src/modules/visit-history/application/services/visit-history.service.ts
@@ -194,11 +194,12 @@ export class VisitHistoryService {
   * Retorna visitantes presentes e visitantes de hoje
   */
   async getVisitorsActivity() {
-    const startOfDay = new Date();
-    startOfDay.setHours(0, 0, 0, 0);
+    const now = getCurrentUtcDate();
+    const startOfDay = new Date(now);
+    startOfDay.setUTCHours(0, 0, 0, 0);
 
-    const endOfDay = new Date();
-    endOfDay.setHours(23, 59, 59, 999);
+    const endOfDay = new Date(now);
+    endOfDay.setUTCHours(23, 59, 59, 999);
 
     // Visitantes Presentes: último histórico sem saída
     const visitorsPresent = await this.visitHistoryRepository.findVisitorsPresent();
